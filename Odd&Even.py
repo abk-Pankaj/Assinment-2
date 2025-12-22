5c81e9d33bf87f626e5b81cfd54095502904f72f
Pankaj <zodiacshub@gmail.com>
2025-12-22 22:13:09 +0530

    Add files via upload

---
+++ Odd&Even.py
@@ -0,0 +1,6 @@
+given_number = int((input("Enter Your number : ")))
+
+if given_number%2 == 0:
+    print(f"{given_number} is an even number")
+else:
+    print(f"{given_number} is an odd number")
---
+++ Sumofno.py
@@ -0,0 +1,8 @@
+Given_number = 50
+
+Sum_first_50th_is = (Given_number * (Given_number + 1) // 2)
+print("Sum of first 50 natural numbers is:", Sum_first_50th_is)
+
+
+    
+   
