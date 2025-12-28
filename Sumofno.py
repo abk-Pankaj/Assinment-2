9d0114cb701e752433ae857b98b29fec6c5614d7
Pankaj <zodiacshub@gmail.com>
2025-12-28 16:02:58 +0530

    Refactor sum calculation and output format
    Removed intermediate print statements and adjusted final output format.

--- Sumofno.py
+++ Sumofno.py
@@ -1,10 +1,9 @@
 Given_number = 50
 
 for i in range(1, Given_number + 1):
-    print(i, end=" ")
-
     Sum_first_50th_is = (i * (i + 1) // 2)
-    print("Sum of first 50 natural numbers is:", Sum_first_50th_is)
+    
+print(f"The sum of number from 1 to {Given_number} is : {Sum_first_50th_is}")
 
 
     
