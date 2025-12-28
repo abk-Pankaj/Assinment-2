313e16d99697c7573e7efc1820b7b2efde8241ad
Pankaj <zodiacshub@gmail.com>
2025-12-28 15:57:16 +0530

    Refactor Sumofno.py to display cumulative sums
    Modified the script to print each natural number up to 50 and its cumulative sum.

--- Sumofno.py
+++ Sumofno.py
@@ -1,7 +1,10 @@
 Given_number = 50
 
-Sum_first_50th_is = (Given_number * (Given_number + 1) // 2)
-print("Sum of first 50 natural numbers is:", Sum_first_50th_is)
+for i in range(1, Given_number + 1):
+    print(i, end=" ")
+
+    Sum_first_50th_is = (i * (i + 1) // 2)
+    print("Sum of first 50 natural numbers is:", Sum_first_50th_is)
 
 
     
